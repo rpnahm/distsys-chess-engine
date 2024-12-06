b0657fb80fd02fabd122b667e0a13adc739d7717
Ryan Nahm <rnahm@student10.cse.nd.edu>
2024-12-05 23:11:04 -0500

    built testing script

---
+++ cmd/test/main.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/notnil/chess"
+	"github.com/notnil/chess/uci"
+	"github.com/rpnahm/distsys-chess-engine/pkg/client"
+	"github.com/rpnahm/distsys-chess-engine/pkg/common"
+)
+
+func main() {
+	// handle command line input
+	if len(os.Args) != 6 {
+		log.Fatal("Usage: ./server <BaseServerName> <numServers> <turnTime(ms)> <numGames> <threads>")
+	}
+	nServers, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatal("Must use integer for numServers", err)
+	}
+	t, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		log.Fatal("Must use integer for turn time", err)
+	}
+	turnTime := time.Duration(t) * time.Millisecond
+	nGames, err := strconv.Atoi(os.Args[4])
+	if err != nil {
+		log.Fatal("Must use integer for nGames", err)
+	}
+	nThreads, err := strconv.Atoi(os.Args[5])
+	if err != nil {
+		log.Fatal("Must use integer for nThreads", err)
+	}
+
+	// Start up engines
+	fmt.Println("Starting up engines")
+	client := client.Init(os.Args[1], nServers, turnTime, 50*time.Millisecond)
+
+	localEng, err := uci.New("bin/stockfish")
+	if err != nil {
+		log.Fatal("Unable to start local stockfish")
+	}
+
+	defer localEng.Close()
+
+	// Connecting to all servers
+	fmt.Println("Connecting to all servers")
+	err = client.ConnectAll()
+	if err != nil {
+		log.Fatal("Unable to connect to all servers", err)
+	}
+	defer client.Shutdown()
+
+	// tracking information
+	systemWins, systemDraws, systemLosses := 0, 0, 0
+	fd, err := os.OpenFile(fmt.Sprintf("%s-%d-%d-%d.log", os.Args[1], nServers, t, nThreads), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatal("Unable to open log file", err)
+	}
+	defer fd.Close()
+	fd.WriteString("ServerNodesProcessed, ClientNodesProcessed\n")
+
+	var results common.Results
+
+	fmt.Println("All servers connected")
+	// games loop
+	for game := 0; game < nGames; game++ {
+		//setup each game
+		// options
+		var options []uci.CmdSetOption
+		options = append(options, uci.CmdSetOption{Name: "Threads", Value: fmt.Sprint(nThreads)})
+		options = append(options, uci.CmdSetOption{Name: "Hash", Value: fmt.Sprint((10240 * nThreads))})
+
+		err = localEng.Run(uci.CmdUCI, uci.CmdIsReady, uci.CmdUCINewGame)
+		if err != nil {
+			log.Fatal("Unable configure local stockfish", err)
+		}
+		var cmds []uci.Cmd
+		for _, c := range options {
+			cmds = append(cmds, c)
+		}
+		err = localEng.Run(cmds...)
+		if err != nil {
+			log.Fatal("Unable to run setoptions on local engine", err)
+		}
+
+		fmt.Println("Remotely creating a newgame")
+		client.Game = *chess.NewGame(chess.UseNotation(chess.UCINotation{}))
+
+		err = client.NewGame(*client.Game.Position(), options)
+		if err != nil {
+			log.Fatal("Unable to start newgames on servers", err)
+		}
+
+		fmt.Println("Entering Game Loop")
+		// game loop
+		for client.Game.Outcome() == chess.NoOutcome {
+			// Local Move
+			if game%2 == 0 {
+
+				cmdPos := uci.CmdPosition{Position: client.Game.Position()}
+				cmdGo := uci.CmdGo{MoveTime: turnTime}
+				localEng.Run(cmdPos, cmdGo)
+
+				move := localEng.SearchResults().BestMove
+				client.Game.Move(move)
+
+				if client.Game.Outcome() != chess.NoOutcome {
+					break
+				}
+
+				// server move
+				results, _ = client.Run()
+			} else {
+				// server move
+				results, _ = client.Run()
+
+				if client.Game.Outcome() != chess.NoOutcome {
+					break
+				}
+
+				cmdPos := uci.CmdPosition{Position: client.Game.Position()}
+				cmdGo := uci.CmdGo{MoveTime: turnTime}
+				localEng.Run(cmdPos, cmdGo)
+
+				move := localEng.SearchResults().BestMove
+				client.Game.Move(move)
+			}
+			fd.WriteString(fmt.Sprintf("%d, %d\n", results.Nodes, localEng.SearchResults().Info.Nodes))
+
+		}
+
+		log.Println(client.Game.Outcome(), client.Game.Method(), len(client.Game.MoveHistory()), "moves")
+		if client.Game.Outcome() == chess.Draw {
+			systemDraws++
+		} else if game%2 == 0 {
+			if client.Game.Outcome() == chess.WhiteWon {
+				systemLosses++
+			} else {
+				systemWins++
+			}
+		} else {
+			if client.Game.Outcome() == chess.BlackWon {
+				systemWins++
+			} else {
+				systemLosses++
+			}
+		}
+	}
+	fd.WriteString(fmt.Sprintf("Distributed Chess Record against Local Stockfish:\n%d-%d-%d\n", systemWins, systemDraws, systemLosses))
+}
--- makefile
+++ makefile
@@ -6,9 +6,11 @@ SRC_PATH = cmd
 SERVER_BIN = $(BINARY_PATH)/server
 CLIENT_BIN = $(BINARY_PATH)/client
 STOCKFISH_BIN = $(BINARY_PATH)/stockfish
+TEST_BIN = $(BINARY_PATH)/test
 
 SERVER_SRC = $(SRC_PATH)/server/main.go
 CLIENT_SRC = $(SRC_PATH)/client/main.go
+TEST_SRC = $(SRC_PATH)/test/main.go
 STOCKFISH_PATH = Stockfish/src
 
 UTILS = pkg
@@ -20,11 +22,16 @@ server: $(SERVER_BIN)
 
 client: $(CLIENT_BIN)
 
+test: $(TEST_BIN)
+
 run-server: $(SERVER_BIN)
 	./$(SERVER_BIN) test-rnahm-00
 
 run-client: $(CLIENT_BIN)
 	./$(CLIENT_BIN) test-rnahm
+
+run-test: $(TEST_BIN)
+	./$(TEST_BIN) rnahm 2 3 10 1
 
 $(CLIENT_BIN): $(CLIENT_SRC) $(UTILS)/client/* $(UTILS)/common/* $(BINARY_PATH)
 	$(GO) -o $@ $<
@@ -32,6 +39,9 @@ $(CLIENT_BIN): $(CLIENT_SRC) $(UTILS)/client/* $(UTILS)/common/* $(BINARY_PATH)
 $(SERVER_BIN): $(SERVER_SRC) $(UTILS)/server/* $(UTILS)/common/* $(BINARY_PATH)
 	$(GO) -o $@ $<
 
+$(TEST_BIN): $(TEST_SRC) $(UTILS)/client/* $(UTILS)/common/* $(BINARY_PATH)
+	$(GO) -o $@ $<
+	
 $(STOCKFISH_BIN): $(BINARY_PATH)
 	make -C $(STOCKFISH_PATH) -j profile-build
 	cp $(STOCKFISH_PATH)/stockfish $(STOCKFISH_BIN)
