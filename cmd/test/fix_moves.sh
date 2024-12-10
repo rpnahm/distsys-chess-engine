6d44290b57807088c1e4cc91dc1b37811a4e85e9
Ryan Nahm <rnahm@student10.cse.nd.edu>
2024-12-09 22:50:13 -0500

    added qsub scripts to repo

---
+++ cmd/test/fix_moves.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#$ -pe smp 5         # Specify parallel environment and legal core size
+#$ -q long           # Specify queue
+#$ -N server-fix-moves       # Specify job name
+#$ -t 1-16	 	# Specify number of copies
+
+module use -a ~/privatemodules
+module load golang/1.20      # Required modules
+
+ # Application to execute
+cd ~/distsys-chess-engine
+formatted_id=$(printf "%02d" "$((SGE_TASK_ID - 1))")
+./bin/server "fix-moves-$formatted_id"
